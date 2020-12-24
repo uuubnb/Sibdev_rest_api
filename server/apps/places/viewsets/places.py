8a2c4a0aab597d216e0891a01a73ab1ce8b91b61
uuubnb <antontit89@gmail.com>
2020-12-25 00:20:35 +0700

    Finished task_7

--- server/apps/places/viewsets/places.py
+++ server/apps/places/viewsets/places.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from apps.places.models import Place, Ingredient, Meal
 from apps.places.serializers import PlaceSerializer, IngredientSerializer, MealSerializer
-from url_filter.integrations.drf import DjangoFilterBackend
-from url_filter.filtersets import ModelFilterSet
+from apps.places.filters import IngredientFilterSet, MealFilterSet, IngredientFilterBackend, \
+    MealFilterBackend, DjangoFilterDescriptionInspector
 from rest_framework import permissions
 from apps.main.permissions.main import IsOwnerOrReadOnly, IsPlaceOwnerOrReadOnly
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
@@ -16,28 +18,34 @@ class PlaceViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 
-class IngredientFilterSet(ModelFilterSet):
-    class Meta(object):
-        model = Ingredient
-
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    filter_inspectors=[DjangoFilterDescriptionInspector],
+    operation_description='''
+    Получение списка всех ингридиентов и фильтрация по запросам:
+    - по id блюда - отображает все ингридиенты в этом блюде;
+    - по количеству калорий - отображает все ингридиенты с указанным количеством калорий 
+    '''
+))
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [IngredientFilterBackend]
     filter_class = IngredientFilterSet
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-class MealFilterSet(ModelFilterSet):
-    class Meta(object):
-        model = Meal
-
-
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    filter_inspectors=[DjangoFilterDescriptionInspector],
+    operation_description='''
+    Получение списка всех ингридиентов и фильтрация по запросам:
+    - по id заведения - отображает все блюда в указанном заведении;
+    - по ингридиенту - отображает все блюда с указанным ингридиентом
+    '''
+))
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [MealFilterBackend]
     filter_class = MealFilterSet
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsPlaceOwnerOrReadOnly]
 
